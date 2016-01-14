c09e02815665a229578719a1fa8b86257547655c
christopf <christopf@posteo.de>
2016-01-14 18:31:27 +0100

    self-written scripts

---
+++ Scripts/Bewegungskraefte.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class Bewegungskraefte : MonoBehaviour {
+
+    public float walkSpeed = 13f;
+    public float turnSpeed = 1.95f;
+    public float jumpForce = 25f;
+
+    Vector3 moveAmnount;
+    Vector3 smoothMoveVelocity;
+    Quaternion rotationAmount;
+
+    private bool grounded;
+    public LayerMask groundMask;
+
+    Rigidbody body;
+
+    //Mous
+    //Transform kamera;
+
+	void Start () {
+
+        //kamera = Camera.main.transform;
+        body = GetComponent<Rigidbody>();
+	}
+
+    void Update() {
+
+        //WASD
+        //forward + backwards
+        Vector3 moveDir = new Vector3(0, 0, Input.GetAxisRaw("Vertical")).normalized;
+        Vector3 targetMoveAmount = moveDir * walkSpeed;
+        moveAmnount = Vector3.SmoothDamp(moveAmnount, targetMoveAmount, ref smoothMoveVelocity, .15f);
+
+        //left + right
+        Vector3 horizontalInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0).normalized;
+        float desiredAngle = 49f;
+        desiredAngle *= horizontalInput.x;
+        rotationAmount = Quaternion.AngleAxis(desiredAngle, body.transform.up);
+
+        //up
+        if (Input.GetButtonDown("Jump")) {
+
+            if (grounded) {
+                body.AddForce(body.transform.up * jumpForce, ForceMode.VelocityChange);
+            }
+        }
+
+        grounded = false;
+        Ray charles = new Ray(transform.position, -transform.up);
+        RaycastHit wonder;
+        if (Physics.Raycast(charles, out wonder, 1.1f, groundMask)) {
+
+            grounded = true;
+        }
+
+        //MOUS
+    }
+	
+	void FixedUpdate() {
+
+        body.MovePosition(body.position + GetComponent<Transform>().TransformDirection(moveAmnount) * Time.fixedDeltaTime);
+        body.MoveRotation(Quaternion.Slerp(body.rotation, rotationAmount * body.rotation, Time.fixedDeltaTime * turnSpeed));
+	}
+}
---
+++ Scripts/FollowTheTarget.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class FollowTheTarget : MonoBehaviour {
+
+    GameObject igor; 
+
+    [SerializeField] private float camDistance = 42f;
+    [SerializeField] private float camUp = 10f;
+    [SerializeField] private float smoothy1 = 1.2f;
+//  [SerializeField] private float smoothy2 = 5f;
+    [SerializeField] private Transform foollow;
+
+    private Vector3 targetPosition;
+
+    void Awake() {
+
+        //finding the soul to follow
+        foollow = GameObject.Find("Soul").transform;
+    }
+
+	void Start () {
+	    
+	}
+	
+	void FixedUpdate () {
+
+        // Maybe got to split this values up to get independent rotation/adjusting speed 
+        // for vertical and horizontal.
+
+        //getting in position
+        /*  classic  */
+        targetPosition = foollow.position + foollow.up * camUp - foollow.forward * camDistance;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smoothy1);
+        
+        /*  2.0  // too much jittering cause of the vertex positions
+        targetPosition = foollow.position + foollow.up * camUp;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smoothy1);
+        targetPosition = foollow.position - foollow.forward * camDistance;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smoothy2);
+        */
+
+        //adjust rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(foollow.position - transform.position, foollow.up), 0.15f);
+	}
+}
