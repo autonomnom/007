b444e2f20832d8a218e2298c5b3a0f0473e0834c
christopf <christopf@posteo.de>
2016-01-19 20:04:32 +0100

    the old downlogic script

---
+++ Scripts/Old/DownLogicOLD.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+	
+[RequireComponent(typeof(CheckMeshesAndFindTheNearest))]
+[RequireComponent(typeof(Rigidbody))]
+
+public class DownLogic : MonoBehaviour {
+
+    private Vector3 chooseGooseNorm;
+    private Vector3 chooseGoosePoint;
+    private Vector3 chooseGoose;
+
+    //script for finding the right normal
+    CheckMeshesAndFindTheNearest chekMesh;
+
+    public float gravity = -250f;
+    Rigidbody body;
+
+    void Awake() {
+
+        chekMesh = GetComponent<CheckMeshesAndFindTheNearest>();
+
+        body = GetComponent<Rigidbody>();
+        body.constraints = RigidbodyConstraints.FreezeRotation;
+        body.useGravity = false;
+    }
+
+	void Start () {
+
+	}
+	
+	void Update () {
+	
+	}
+
+    void FixedUpdate() {
+
+        //applying rotation based on the triangles normal below
+        chooseGooseNorm = chekMesh.theNorm;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.FromToRotation(transform.up, chooseGooseNorm) * transform.rotation, .5f);
+        body.AddForce(chooseGooseNorm * gravity);
+    }
+}
